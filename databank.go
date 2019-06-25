2878b6a06cdab8f86841ac7bfd8b60ab1b794e6b
fabarj4 <fabarj4@gmail.com>
2019-06-25 19:33:27 +0700

    commit permtama kali

---
+++ databank.go
@@ -0,0 +1,48 @@
+package gogetdatabank
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+type Data struct {
+	Label string `json:"label"`
+	Value string `json:"value"`
+}
+
+func DataBankHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method == http.MethodGet {
+		dataBank, err := getDataBank()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		result, err := json.Marshal(dataBank)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Write(result)
+		return
+	}
+	http.Error(w, "", http.StatusBadRequest)
+}
+
+func getDataBank() ([]*Data, error) {
+	// var result string
+	url := "https://www.atmbersama.com/layanan"
+	req, _ := http.NewRequest(http.MethodGet, url, nil)
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	temp := string(body)
+	jsonString := temp[48812:52467]
+	var result []*Data
+	if err := json.Unmarshal([]byte(jsonString), &result); err != nil {
+		return result, err
+	}
+	return result, nil
+}
---
+++ example/example.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/fabarj4/gogetdatabank"
+)
+
+func main() {
+	http.HandleFunc("/databank/", gogetdatabank.DataBankHandler)
+	fmt.Println("starting web server at http://localhost:8080/")
+	http.ListenAndServe(":8080", nil)
+}
